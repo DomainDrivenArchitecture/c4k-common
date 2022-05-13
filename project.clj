6ef7bd91ab01ed7f6ce12f0a93a4930138c23f03
bom <mattis.boeckle@meissa-gmbh.de>
2022-05-13 15:38:32 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-common-clj "2.0.3"
+(defproject org.domaindrivenarchitecture/c4k-common-clj "2.0.4-SNAPSHOT"
   :description "Contains predicates and tools for c4k"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
