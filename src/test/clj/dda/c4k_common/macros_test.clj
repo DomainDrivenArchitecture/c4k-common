1227a8a76fb8cd842e876756be22d3f9cad1ad59
bom <mattis.boeckle@meissa-gmbh.de>
2023-11-10 13:26:16 +0100

    Add another inline_resources macro test

--- src/test/clj/dda/c4k_common/macros_test.clj
+++ src/test/clj/dda/c4k_common/macros_test.clj
@@ -3,5 +3,11 @@
    [clojure.test :refer [deftest is are testing run-tests]]
    [dda.c4k-common.macros :refer [inline-resources]]))
 
-(deftest should-inline-resources
+(deftest should-count-inline-resources
   (is (= 3 (count (inline-resources "ingress")))))
+
+(deftest should-inline-resources
+  (let [resource-path (fn [name] (str "dda/c4k_common/inline_resources_test/" name))]
+    (is (= "1" (get (inline-resources "dda/c4k_common/inline_resources_test") (resource-path "inline_resource_1.yaml"))))
+    (is (= "2" (get (inline-resources "dda/c4k_common/inline_resources_test") (resource-path "inline_resource_2.yaml"))))
+    (is (= "3" (get (inline-resources "dda/c4k_common/inline_resources_test") (resource-path "inline_resource_3.yaml"))))))
