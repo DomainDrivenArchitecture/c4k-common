7c21211eaca8deb3decc603ab3da49a44fef764d
bom <mattis.boeckle@meissa-gmbh.de>
2024-01-19 11:44:23 +0100

    release: 6.1.1

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.domaindrivenarchitecture/c4k-common-clj "6.1.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-common-clj "6.1.1"
   :description "Contains predicates and tools for c4k"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
-                 [org.clojure/tools.reader "1.3.6"]
+                 [org.clojure/tools.reader "1.3.7"]
                  [aero "1.1.6"]
                  [orchestra "2021.01.01-1"]
                  [expound "0.9.0"]
@@ -26,9 +26,9 @@
                        :main dda.c4k-common.uberjar
                        :uberjar-name "c4k-common-standalone.jar"
                        :dependencies [[org.clojure/tools.cli "1.0.219"]
-                                      [ch.qos.logback/logback-classic "1.4.11"
+                                      [ch.qos.logback/logback-classic "1.4.14"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "2.0.9"]]}}
+                                      [org.slf4j/jcl-over-slf4j "2.0.11"]]}}
   :release-tasks [["test"]
                   ["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
