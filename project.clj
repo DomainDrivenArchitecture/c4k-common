0a84b7ad6367bf6b412dbb18a51fc8ca9969b8f0
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-02-16 17:50:55 +0100

    release: 6.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-common-clj "6.1.3-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-common-clj "6.1.3"
   :description "Contains predicates and tools for c4k"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
