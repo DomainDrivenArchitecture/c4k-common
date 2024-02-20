a14b7640daa8ca7002eda637ee76e6b8ce7e2959
Michael Jerger <michael.jerger@meissa-gmbh.de>
2024-02-20 16:13:36 +0100

    update deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.11.1" :scope "provided"]
-                 [org.clojure/tools.reader "1.3.7"]
+                 [org.clojure/tools.reader "1.4.0"]
                  [aero "1.1.6"]
                  [orchestra "2021.01.01-1"]
                  [expound "0.9.0"]
@@ -25,8 +25,8 @@
              :uberjar {:aot :all
                        :main dda.c4k-common.uberjar
                        :uberjar-name "c4k-common-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "1.0.219"]
-                                      [ch.qos.logback/logback-classic "1.4.14"
+                       :dependencies [[org.clojure/tools.cli "1.1.230"]
+                                      [ch.qos.logback/logback-classic "1.5.0"
                                        :exclusions [com.sun.mail/javax.mail]]
                                       [org.slf4j/jcl-over-slf4j "2.0.12"]]}}
   :release-tasks [["test"]
