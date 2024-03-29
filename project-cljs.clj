f33b86125a50c5fa3e0bc19a0e57f3e5ab1aca24
bom <mattis.boeckle@meissa-gmbh.de>
2024-03-29 10:26:18 +0100

    release: 6.2.3

--- project-cljs.clj
+++ project-cljs.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-common-cljs "6.2.3-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/c4k-common-cljs "6.2.3"
   :description "Contains predicates and tools for c4k"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
