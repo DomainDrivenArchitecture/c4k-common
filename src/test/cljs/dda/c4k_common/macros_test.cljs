ac285f8c40bb0b8f20e50fe9c3a359274936e6a0
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-06-04 13:06:46 +0200

    fix test

--- src/test/cljs/dda/c4k_common/macros_test.cljs
+++ src/test/cljs/dda/c4k_common/macros_test.cljs
@@ -4,7 +4,7 @@
    [dda.c4k-common.macros :refer-macros [inline-resources]]))
 
 (deftest should-count-inline-resources
-  (is (= 4 (count (inline-resources "ingress")))))
+  (is (= 5 (count (inline-resources "ingress")))))
 
 (deftest should-inline-resources
   (let [resource-path (fn [name] (str "dda/c4k_common/inline_resources_test/" name))
